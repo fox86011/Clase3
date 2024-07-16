63632613d34398340ca49f483b55a09fd5c14c0f
andychalacan <168015224+andychalacan@users.noreply.github.com>
2024-07-16 18:14:48 -0500

    Add files via upload

---
+++ #suma de arreglos.py
@@ -0,0 +1,15 @@
+#suma de arreglos
+filas=[]
+columnas=[]
+numestu=10
+
+
+filas=int(input("Ingrese la base para la suma: "))
+
+columnas=int(input("Ingrese el numero para la suma: "))
+
+res1=filas
+res2=columnas
+print(filas)
+print(columnas)
+print(filas+columnas)
